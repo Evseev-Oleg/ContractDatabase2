4a1962941c8d725792184daa2d7fe2b6b981f274
Evseev Oleg <oleg063053@gmail.com>
2021-08-10 20:24:08 +0400

    no commit

--- src/main/java/ru/testTask/client/ClientLoader.java
+++ src/main/java/ru/testTask/client/ClientLoader.java
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 public class ClientLoader {
     private Socket socket;
 
+
     /**
      * метод создающий соединение с сервером
      */
@@ -30,7 +31,7 @@ public class ClientLoader {
      * строкой JOIN
      * применяется OutputStreamWriter
      *
-     * @param contract
+     * @param contract документ
      */
     public void handle(Contract contract) {
         Converter converter = new Converter();
